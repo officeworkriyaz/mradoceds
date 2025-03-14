7c8473a6ad8bef3bd720cd7e51d51d95befe4f2f
officeworkriyaz <officeworkriyaz@gmail.com>
2025-03-14 17:23:56 +0530

    bug fixed

--- blocks/apidemo/apidemo.js
+++ blocks/apidemo/apidemo.js
@@ -7,7 +7,7 @@ export default async function decorate(block) {
 }
 
 async function fetchData(block) {
-	const response = await fetch ('https://jsonplaceholder.typicode.com/todos/1').thne(res => res.clone().json())
+	const response = await fetch ('https://jsonplaceholder.typicode.com/todos/1').then(res => res.clone().json())
 block.append(response.title);
 console.log(response);
 }
