0864eea72c8b78a5e45588b9bc5cd442183dd2cb
officeworkriyaz <officeworkriyaz@gmail.com>
2025-03-26 21:10:45 +0530

    cursor ai fixed the bug

--- blocks/apidemo/apidemo.js
+++ blocks/apidemo/apidemo.js
@@ -1,11 +1,10 @@
 import fetch from 'node-fetch';
 
 async function fetchData(block) {
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1').then(res => res.clone().json());
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1').then((res) => res.clone().json());
   block.append(response.title);
 }
 export default async function decorate(block) {
   fetchData(block);
 }
-
 
