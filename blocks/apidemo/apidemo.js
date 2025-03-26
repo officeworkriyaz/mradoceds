4368de7cfa5e3e7a810ce601af0d4aec8d67299e
officeworkriyaz <officeworkriyaz@gmail.com>
2025-03-26 21:00:40 +0530

    order changed

--- blocks/apidemo/apidemo.js
+++ blocks/apidemo/apidemo.js
@@ -1,10 +1,11 @@
 import fetch from 'node-fetch';
 
-export default async function decorate(block) {
-  fetchData(block);
-}
-
 async function fetchData(block) {
   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1').then(res => res.clone().json());
   block.append(response.title);
 }
+export default async function decorate(block) {
+  fetchData(block);
+}
+
+
