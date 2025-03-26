2b4a78733dc79c16092aa4dbc5753a24c01ed51e
officeworkriyaz <officeworkriyaz@gmail.com>
2025-03-26 20:44:24 +0530

    fix

--- blocks/apidemo/apidemo.js
+++ blocks/apidemo/apidemo.js
@@ -1,8 +1,9 @@
+import { fetch } from "../../scripts/aem.js";
 export default async function decorate(block) {
   fetchData(block);
 }
 
 async function fetchData(block) {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1").then(res => res.clone().json())
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1').then(res => res.clone().json());
   block.append(response.title);
 }
